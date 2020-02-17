207f3343c9b4cd370e2640a4476014c588433cd7
admin <ejeksenov>
2020-02-17 11:49:46 +0600

    Initial commit

---
+++ app/src/main/java/kz/coffee/go/MainActivity.kt
@@ -0,0 +1,12 @@
+package kz.coffee.go
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
